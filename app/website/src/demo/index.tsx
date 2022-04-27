3b60a6d56979024300b17f85471e1dcd1b4082ae
arvin.xiang <arvin.xiang@ubtrobot.com>
2022-04-27 14:28:37 +0800

    fix: 修复Demo语法错误
    Change-Id: Ia2ae3d3bfcae688c3d22f7e55df6a3263bebc6f3

--- app/website/src/demo/index.tsx
+++ app/website/src/demo/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { attachPropertiesToComponent } from '@ubt/udesign-utils';
 import { Doc } from './doc';
 import { Page } from './page';
 import { Block } from './block';
-import { attachPropertiesToComponent } from '@ubt/udesign-utils';
 
-export const Demo = attachPropertiesToComponent(<></>, {
+export const Demo = attachPropertiesToComponent(() => <></>, {
   Doc,
   Page,
   Block,
