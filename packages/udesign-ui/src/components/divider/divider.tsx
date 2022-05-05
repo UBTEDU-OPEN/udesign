a9532180921288fcc39f519f183d2bac0409b368
arvin.xiang <arvin.xiang@ubtrobot.com>
2022-05-05 17:40:21 +0800

    refactor(divider): 重构Divider组件
    Change-Id: I97c101ee68c510b0958f698d207d559da7dfc9fe

--- packages/udesign-ui/src/components/divider/divider.tsx
+++ packages/udesign-ui/src/components/divider/divider.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { NativeProps } from '../../utils';
+import { Direction } from '../../constants';
 
 const prefixCls = `ud-divider`;
 
@@ -9,31 +10,36 @@ export type DividerProps = {
   orientation?: 'left' | 'right' | 'center'; //分割线标题的位置
   orientationMargin?: string | number; //标题和最近 left/right 边框之间的距离，去除了分割线，同时 orientation 必须为 left 或 right
   plain?: boolean; //文字是否显示为普通正文样式
-  type?: 'horizontal' | 'vertical'; //水平还是垂直类型
+  type?: Direction; //水平还是垂直类型
 } & NativeProps;
 
-export const Divider = ({ dashed , orientation = 'center', orientationMargin, plain = false, type = 'horizontal', children, className, style }: DividerProps) => {
-  const textRef = useRef<HTMLSpanElement>(null);
-
+export const Divider = ({ dashed, orientation = 'center', orientationMargin, plain, type = 'horizontal', children, className, style }: DividerProps) => {
   const cls = classNames(
     prefixCls,
     {
-      [`${prefixCls}-dashed`]: dashed,
-      [`${prefixCls}-text`]: children ,
-      [`${prefixCls}-text-${orientation}`]: orientation,
       [`${prefixCls}-${type}`]: type,
       [`${prefixCls}-plain`]: plain,
-      [`${prefixCls}-orientation-margin-${orientation}`]: orientationMargin,
+      [`${prefixCls}-dashed`]: dashed,
+      [`${prefixCls}-with-text`]: children,
+      [`${prefixCls}-with-text-${orientation}`]: orientation,
+      [`${prefixCls}-no-default-orientation-margin-${orientation}`]: orientationMargin !== undefined,
     },
     className,
   );
 
+  const getMarginStyle = () => {
+    if (orientationMargin === undefined) return;
+
+    return orientation === 'left' ? { marginLeft: `${orientationMargin}px` } : { marginRight: `${orientationMargin}px` };
+  };
 
   return (
     <div className={cls} style={style}>
-      <span ref={textRef} className='text' style={orientation==='left' ? {marginLeft: `${orientationMargin}px`} : {marginRight: `${orientationMargin}px`}}>
-        {children}
-      </span>
+      {children ? (
+        <span className={`${prefixCls}-inner-text`} style={getMarginStyle()}>
+          {children}
+        </span>
+      ) : null}
     </div>
   );
 };
