68ba245f81cb3aac64bacf4cc892b9787512c3bc
xianggang.shen <xianggang.shen@ubtrobot.com>
2022-07-13 10:19:12 +0800

    fix: 优化样式
    Change-Id: I964e4f8e5f79ad3c5445a3a008dd85ccb5d01995

--- packages/udesign-ui/src/components/layout/sider.tsx
+++ packages/udesign-ui/src/components/layout/sider.tsx
@@ -9,7 +9,6 @@ export type CollapseType = 'clickTrigger' | 'responsive';
 
 export type SiderProps = {
   width?: number | string; // 宽度
-  theme?: 'light' | 'dark'; // 主题颜色
   collapsible?: boolean; // 是否可收起
   trigger?: ReactNode; // 自定义 trigger，设置为 null 时隐藏 trigger
   collapsedWidth?: number; // 收缩宽度，设置为 0 会出现特殊 trigger
@@ -17,7 +16,7 @@ export type SiderProps = {
   onCollapse?: (collapsed: boolean, type: CollapseType) => void; // 展开-收起时的回调函数
   collapsed?: boolean; // 当前收起状态
 } & NativeProps;
-export const Sider = ({ width = 200, theme = 'dark', defaultCollapsed = false, style, collapsible = false, trigger, collapsedWidth = 100, onCollapse, children, className, ...props }: SiderProps) => {
+export const Sider = ({ width = 200, defaultCollapsed = false, style, collapsible = false, trigger, collapsedWidth = 100, onCollapse, children, className, ...props }: SiderProps) => {
   const [toggle, steToggle] = useState(false);
   const [stateWidth, setStateWidth] = useState(width);
 
@@ -28,7 +27,6 @@ export const Sider = ({ width = 200, theme = 'dark', defaultCollapsed = false, s
   const cls = classNames(
     prefixCls,
     {
-      [`${prefixCls}-theme-${theme}`]: theme,
       [`${prefixCls}-toggle`]: toggle,
     },
     className,
