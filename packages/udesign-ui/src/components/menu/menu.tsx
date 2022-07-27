1d268b05dc436c13558a2ca5703a66e702f39f38
xianggang.shen <xianggang.shen@ubtrobot.com>
2022-07-27 15:09:13 +0800

    feat: 新增 menu组件
    Change-Id: I2872f961142e4d49f3fc8f890ef139a92706a9a8

--- packages/udesign-ui/src/components/menu/menu.tsx
+++ packages/udesign-ui/src/components/menu/menu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 import { NativeProps, usePropsValue } from '../../utils';
 import MenuContext, { MenuMode } from './context';
@@ -8,27 +8,47 @@ const prefixCls = `${BASE_CLASS_PREFIX}-menu`;
 
 export type MenuProps = {
   isCollapsed?: boolean; // 设置是否折叠
+  hasLine?: boolean; // 设置悬停或选中时出现线条
   mode?: MenuMode; // 水平 or 垂直
   activeKey?: string; // 当前选中的菜单项
   defaultActiveKey?: string; // 默认选中的菜单项
+  subActiveKey?: string; // 当前选中的菜单项
+  defaultSubActiveKey?: string; // 默认选中的菜单项
+  radius?: boolean;
+  onlyOne?: boolean; // 只展开一个
   onChange?: (name: string) => void; // 选中的菜单项变化时触发
 } & NativeProps;
 
-export const Menu = ({ isCollapsed = false, mode = 'vertical', onChange, className, children, ...props }: MenuProps) => {
+export const Menu = ({ isCollapsed = false, mode = 'vertical', hasLine, onlyOne, onChange, className, radius, children, style, ...props }: MenuProps) => {
   const [activeKey, setActiveKey] = usePropsValue({
     value: props.activeKey,
     defaultValue: props.defaultActiveKey ?? '',
     onChange,
   });
+  const [subActiveKey, setSubActiveKey] = usePropsValue({
+    value: props.subActiveKey,
+    defaultValue: props.defaultSubActiveKey ?? '',
+    onChange,
+  });
+  const [affectedByChildrenSubActiveKey, setAffectedByChildrenSubActiveKey] = useState('');
+  // affectedByChildren
 
   const onClick = (name: string) => {
     setActiveKey(name);
+  };
+  const handleClick = (name: string) => {
+    setSubActiveKey(name);
+  };
+  const subClick = (name: string, item: string) => {
+    console.log(name, item);
+    setAffectedByChildrenSubActiveKey(item);
   };
 
   const cls = classNames(
     prefixCls,
     {
       [`${prefixCls}-${mode}`]: mode,
+      [`${prefixCls}-is-collapsed`]: isCollapsed,
     },
     className,
   );
@@ -41,11 +61,20 @@ export const Menu = ({ isCollapsed = false, mode = 'vertical', onChange, classNa
           prefixCls,
           isCollapsed,
           activeKey,
+          hasLine,
+          subActiveKey,
+          affectedByChildrenSubActiveKey,
           onClick,
+          handleClick,
+          subClick,
+          onlyOne,
           firstLevel: true,
+          radius,
         }}
       >
-        <ul className={cls}>{children}</ul>
+        <ul className={cls} style={style}>
+          {children}
+        </ul>
       </MenuContext.Provider>
     </>
   );
--- packages/udesign-ui/src/components/menu/submenu.tsx
+++ packages/udesign-ui/src/components/menu/submenu.tsx
@@ -1,21 +1,105 @@
-import React, { useContext } from 'react';
+import React, { useContext, ReactNode, useState } from 'react';
+import { DownOutlined, UpOutlined } from '@ubt/udesign-icons';
+import classNames from 'classnames';
 import { MenuItem, MenuItemProps } from './item';
 import Tooltip from '../tooltip';
 import MenuContext from './context';
+import SubMenuContext from './SubMenuContext';
 
 export type SubmenuProps = MenuItemProps & {
   title: string; // 子菜单显示文案
+  icon?: ReactNode; // 左侧图标
+  expendIcon?: ReactNode; // 展开收起图标
+  subMode?: string;
+  name?: string;
+  subActive?: boolean;
 };
 
-export const Submenu = ({ title, children, ...restProps }: SubmenuProps) => {
+export const Submenu = ({ title, children, icon, subMode, subActive, name = '', style, ...restProps }: SubmenuProps) => {
   const context = useContext(MenuContext);
-  const { mode, prefixCls } = context;
+  const { mode, prefixCls, isCollapsed, subActiveKey, affectedByChildrenSubActiveKey, onlyOne } = context;
+
+  const renderInline = () => {
+    if (onlyOne) {
+      console.log(subActiveKey);
+      let active = subActiveKey === name;
+      const handleClick = () => {
+        context?.handleClick?.(name);
+      };
+      const cls = classNames(prefixCls, {
+        [`${prefixCls}-submenu-header`]: true,
+        [`${prefixCls}-submenu-header-focus`]: active,
+      });
+      const itemCls = classNames(prefixCls, {
+        [`${prefixCls}-submenu-body`]: active,
+        [`${prefixCls}-submenu-body-hidden`]: !active,
+      });
+      return (
+        <>
+          <li className={`${prefixCls}-submenu-wrapper`}>
+            <div className={cls} onClick={handleClick}>
+              <span className={`${prefixCls}-submenu-header-left`}>
+                {icon}
+                <span>{title}</span>
+              </span>
+              <span>{!active ? <DownOutlined /> : <UpOutlined />}</span>
+            </div>
+            <ul className={itemCls}>{children}</ul>
+          </li>
+        </>
+      );
+      // eslint-disable-next-line no-else-return
+    } else {
+      const [active, setActive] = useState(subActive);
+      const [subFocus, setSubFocus] = useState(name);
+      const handleClick = () => {
+        setActive(!active);
+      };
+      const cls = classNames(prefixCls, {
+        [`${prefixCls}-submenu-header`]: true,
+        [`${prefixCls}-submenu-header-focus`]: affectedByChildrenSubActiveKey === subFocus,
+      });
+      const itemCls = classNames(prefixCls, {
+        [`${prefixCls}-submenu-body`]: active,
+        [`${prefixCls}-submenu-body-hidden`]: !active,
+      });
+
+      return (
+        <>
+          <SubMenuContext.Provider
+            value={{
+              name: name === undefined ? '' : name,
+            }}
+          >
+            <li className={`${prefixCls}-submenu-wrapper`}>
+              <div className={cls} onClick={handleClick}>
+                <span className={`${prefixCls}-submenu-header-left`}>
+                  {icon}
+                  <span>{title}</span>
+                </span>
+                <span>{!active ? <DownOutlined /> : <UpOutlined />}</span>
+              </div>
+              <ul className={itemCls}>{children}</ul>
+            </li>
+          </SubMenuContext.Provider>
+        </>
+      );
+    }
+  };
+  const renderTitle = () => <>{!isCollapsed ? <span style={{ flexGrow: '0.9' }}>{title}</span> : null}</>;
 
   return (
     <>
-      <Tooltip prefixCls={`${prefixCls}-submenu`} showArrow={false} content={children} placement={mode === 'horizontal' ? 'bottomLeft' : 'rightTop'} {...restProps}>
-        <MenuItem>{title}</MenuItem>
-      </Tooltip>
+      {(mode === 'inline' || subMode === 'inline') && !isCollapsed ? (
+        renderInline()
+      ) : (
+        <Tooltip className={`${prefixCls}-submenu-horizontal`} prefixCls={`${prefixCls}-submenu`} showArrow={false} content={children} placement={mode === 'horizontal' ? 'bottomLeft' : 'rightTop'} {...restProps} style={{ width: '100%' }}>
+          <MenuItem name={name} style={style} className={isCollapsed ? `${prefixCls}-submenu-is-collapsed` : ``}>
+            {icon}
+            {renderTitle()}
+          </MenuItem>
+        </Tooltip>
+      )}
     </>
   );
 };
