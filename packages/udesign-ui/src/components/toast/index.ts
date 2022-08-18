6ed4441afc2fd42769bccd231e578ef851490518
xianggang.shen <xianggang.shen@ubtrobot.com>
2022-08-18 15:24:49 +0800

    fix: 解决连续点击Toast时，后面等待前面的问题
    Change-Id: If727afb47b8acdb62b02ada040fa3f1b6e58d5d6

--- packages/udesign-ui/src/components/toast/index.ts
+++ packages/udesign-ui/src/components/toast/index.ts
@@ -4,26 +4,21 @@ import { Toast as InnerToast, ToastProps } from './toast';
 
 const element = new PortalWrapper<typeof InnerToast>(InnerToast);
 
-let isShowing = false;
+let timer: NodeJS.Timeout;
 
 export default function Toast(p: ToastProps | string): Promise<void> {
   const props = typeof p === 'string' ? { content: p } : p;
-
   return new Promise((resolve, reject) => {
-    if (isShowing) {
-      resolve();
-      return;
-    }
-    isShowing = true;
+    timer && clearTimeout(timer);
     const duration = props?.duration || 2000;
     const delay = props?.delay ?? 0;
+
     setTimeout(() => {
       element.show({
         ...props,
       });
-      setTimeout(() => {
+      timer = setTimeout(() => {
         element.close();
-        isShowing = false;
         props?.afterClose?.();
       }, duration);
     }, delay);
