0b26663be2c718969a85e972357f1b4e19abbf4b
Alex <alex.tan@ubtrobot.com>
2022-09-02 16:13:49 +0800

    feat(build): 新增 basePath
    Change-Id: Ia266e677064f13412e81e8dae01285c6958cb6db

--- app/website/next.config.js
+++ app/website/next.config.js
@@ -2,10 +2,13 @@ const withImages = require('next-images');
 
 const withTM = require('next-transpile-modules')(['@ubt/udesign-ui', '@ubt/udesign-icons']);
 
+const basePath = process.env.UDESIGN_BASE_PATH || "";
+
 module.exports = withTM(
   withImages({
     // 你项目中其他的 Next.js 配置
     reactStrictMode: true,
     trailingSlash: true,
+    basePath,
   }),
 );
