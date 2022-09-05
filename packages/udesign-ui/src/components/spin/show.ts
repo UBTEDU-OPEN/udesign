0981f5d7e2e9a8c968a741ec3127e8544b281508
xianggang.shen <xianggang.shen@ubtrobot.com>
2022-09-05 18:54:20 +0800

    fix: 优化spin组件
    Change-Id: I914adad4f61cab3c160a5227656033fb21762c72

--- packages/udesign-ui/src/components/spin/show.ts
+++ packages/udesign-ui/src/components/spin/show.ts
@@ -3,13 +3,20 @@ import './spin.scss';
 import { Spin as InnerSpin, SpinProps } from './spin';
 
 const element = new PortalWrapper<typeof InnerSpin>(InnerSpin);
-
+let timer: NodeJS.Timeout;
 export default function showLoading(props: SpinProps) {
+  clearTimeout(timer);
+  const timeOut = props.timeOut || 60;
+
   element.show({
     ...props,
-    timeOut: 60,
+    timeOut: 0,
     _global: true,
   });
+  timer = setTimeout(() => {
+    element.close();
+    console.log(11);
+  }, timeOut * 1000);
 }
 export function hideLoading() {
   element.close();
