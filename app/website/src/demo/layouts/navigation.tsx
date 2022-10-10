d29aca99e914156b34a874fc7bedb52110bceb59
Arvin <webappsniper@gmail.com>
2022-10-10 16:25:57 +0800

    fix: 演示网站点击“设计”会出现一个404页面
    Change-Id: I1d5fa325c691da67ec0b864831950915bbe3bb6f

--- app/website/src/demo/layouts/navigation.tsx
+++ app/website/src/demo/layouts/navigation.tsx
@@ -9,6 +9,8 @@ const Navigation = () => {
   const defaultActiveKey = router.asPath.split('/').shift() || router.asPath.split('/').slice(1, 2)[0];
 
   function handleChange(name: string) {
+    if (name === 'spec') return;
+
     let path = '';
     switch (name) {
       case 'docs':
@@ -31,7 +33,7 @@ const Navigation = () => {
           <a className='font-bold text-xl no-underline'>{SITE_NAME}</a>
         </Link>
         <Menu hasLine className='gap-5' onChange={handleChange} defaultActiveKey={defaultActiveKey} mode='horizontal'>
-          <a className='text-inherit no-underline' target='_blank' href='http://dev.edu.ubtrobot.com/UI/U-Design/' rel='noreferrer'>
+          <a className='text-inherit no-underline' target='_blank' href='http://dev.edu.ubtrobot.com/UI/U-Design/' rel='noopener noreferrer'>
             <Menu.Item name='spec'>设计</Menu.Item>
           </a>
           <Menu.Item name='docs'>文档</Menu.Item>
