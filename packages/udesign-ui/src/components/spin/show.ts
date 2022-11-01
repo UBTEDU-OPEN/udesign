3602d30a16ac4136762ebf34a86fecaabd62a357
xianggang.shen <xianggang.shen@ubtrobot.com>
2022-11-01 15:30:32 +0800

    fix: 修复Spin层较低问题
    Change-Id: Idc9f06be2df82c79e74e9e0007fcdadb6779eb67

--- packages/udesign-ui/src/components/spin/show.ts
+++ packages/udesign-ui/src/components/spin/show.ts
@@ -4,15 +4,16 @@ import { Spin as InnerSpin, SpinProps } from './spin';
 
 const element = new PortalWrapper<typeof InnerSpin>(InnerSpin);
 let timer: NodeJS.Timeout;
-export default function showLoading(props: SpinProps) {
+export default function showLoading(props?: SpinProps) {
   clearTimeout(timer);
-  const timeOut = props.timeOut || 60;
+  const timeOut = props?.timeOut || 60;
 
   element.show({
     ...props,
     timeOut: 0,
     _global: true,
   });
+
   timer = setTimeout(() => {
     element.close();
     console.log(11);
