680d97af0919b825d9273e793a7ddb9a7a8c70d9
Arvin <webappsniper@gmail.com>
2022-11-03 15:07:40 +0800

    fix(website): 修复部分演示页面图片丢失的问题
    Change-Id: Ib915b6a1c8e78cc30b5b7d17b000c767a6759c37

--- app/website/next.config.js
+++ app/website/next.config.js
@@ -1,27 +1,30 @@
 const webpack = require('webpack');
 const pkg = require('./package.json');
 
-const { withContentlayer } = require('next-contentlayer');
 const withTM = require('next-transpile-modules')(['@ubt/udesign-ui', '@ubt/udesign-icons']);
+const withImages = require('next-images');
+const { withContentlayer } = require('next-contentlayer');
 
 const basePath = process.env.UDESIGN_BASE_PATH || '';
 
 module.exports = withTM(
-  withContentlayer({
-    // 你项目中其他的 Next.js 配置
-    reactStrictMode: true,
-    trailingSlash: true,
-    basePath,
-    inlineImageLimit: false,
-    webpack: (config, options) => {
-      // Perform customizations to config
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          'process.env.UDESIGN_VERSION': JSON.stringify(pkg.version),
-        }),
-      );
-      // Important: return the modified config
-      return config;
-    },
-  }),
+  withImages(
+    withContentlayer({
+      // 你项目中其他的 Next.js 配置
+      reactStrictMode: true,
+      trailingSlash: true,
+      basePath,
+      inlineImageLimit: false,
+      webpack: (config, options) => {
+        // Perform customizations to config
+        config.plugins.push(
+          new webpack.DefinePlugin({
+            'process.env.UDESIGN_VERSION': JSON.stringify(pkg.version),
+          }),
+        );
+        // Important: return the modified config
+        return config;
+      },
+    }),
+  ),
 );
