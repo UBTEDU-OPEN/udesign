dd5f4f6d53e939757cda615a75a67a61bb34a2f8
xianggang.shen <xianggang.shen@ubtrobot.com>
2022-11-22 19:56:59 +0800

    fix: 修复Radio组件受控功能
    Change-Id: I07721c55a3cc4ad5e6b5548402d538e19afb5b1b

--- packages/udesign-ui/src/components/radio/button.tsx
+++ packages/udesign-ui/src/components/radio/button.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import classNames from 'classnames';
-import { NativeProps } from '../../utils';
-import { RadioContext, types } from './context';
+import { NativeProps, usePropsValue } from '../../utils';
+import { RadioContext } from './context';
 import { BASE_CLASS_PREFIX } from '../../constants';
 
 export type RadioChangeEventType = React.ChangeEvent<HTMLInputElement>;
@@ -13,62 +13,60 @@ export type RadioProps = {
   checked?: boolean; // 指定当前是否选中。默认值：false
   defaultChecked?: boolean; // 初始是否选中。默认值：false
   disabled?: boolean; // 禁用 Radio。默认值：false
-  value?: string; // 根据 value 进行比较，判断是否选中。默认值：-
+  value?: string; // 根据 value 进行比较，判断是否选中。
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void; // 变化时回调函数。
 } & NativeProps;
 
 const prefixCls = `${BASE_CLASS_PREFIX}-radio-button`;
 
 export const Button = ({ defaultChecked = false, disabled, className, children, value, style, ...restProps }: RadioProps) => {
-  const checked = 'checked' in restProps ? restProps.checked : defaultChecked;
-  const [innerChecked, setInnerChecked] = useState<boolean>(checked!);
+  const [checked, setChecked] = usePropsValue({
+    value: restProps.checked,
+    defaultValue: defaultChecked,
+  });
   const context = useContext(RadioContext);
   const isDisabled = disabled || context.disabled;
   const [cusInput, setCusInput] = useState(
     classNames(`${prefixCls}-cus-input`, {
-      [`${prefixCls}-unchecked`]: !innerChecked,
-      [`${prefixCls}-checked`]: innerChecked,
+      [`${prefixCls}-unchecked`]: !checked,
+      [`${prefixCls}-checked`]: checked,
       [`${prefixCls}-disabled`]: isDisabled,
       [`${prefixCls}-cus-input-common`]: true,
     }),
   );
 
-  function handleClick() {
+  function handleClick(e: React.MouseEvent<HTMLInputElement>) {
     if (isDisabled) return;
-    setInnerChecked(true);
     // 更新value值
-    if (context.dispatch) {
-      context.dispatch({
-        type: types.UPDATE_VALUE,
-        payload: {
-          value,
-        },
-      });
+    if (context.value && value !== undefined) {
+      context.onChange?.(value, e);
+      return;
     }
+    setChecked(true);
   }
   useEffect(() => {
     if (context.value && value) {
-      setInnerChecked(context.value === value);
+      setChecked(context.value === value);
     }
   }, [context.value]);
 
   useEffect(() => {
-    if ('checked' in restProps) {
-      setInnerChecked(restProps.checked!);
-    }
-  }, [restProps.checked]);
-
-  useEffect(() => {
     setCusInput(
       classNames(`${prefixCls}-cus-input`, {
-        [`${prefixCls}-unchecked`]: !innerChecked,
-        [`${prefixCls}-checked`]: innerChecked,
+        [`${prefixCls}-unchecked`]: !checked,
+        [`${prefixCls}-checked`]: checked,
         [`${prefixCls}-disabled`]: isDisabled,
         [`${prefixCls}-cus-input-common`]: true,
       }),
     );
-  }, [innerChecked, disabled, context.disabled]);
+  }, [checked, disabled, context.disabled]);
 
   const cls = classNames(className, prefixCls);
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    restProps.onChange?.(event);
+    // context.onChange?.(event);
+  };
 
   return (
     <>
@@ -77,14 +75,12 @@ export const Button = ({ defaultChecked = false, disabled, className, children, 
           name={context.name}
           type='radio'
           className={`${prefixCls}-hidden`}
-          checked={innerChecked}
+          checked={checked}
           value={value}
           disabled={isDisabled}
           onClick={handleClick}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            if (context.onChange) {
-              context.onChange(event);
-            }
+          onChange={(e) => {
+            restProps.onChange?.(e);
           }}
         />
         <div className={cusInput}>{children}</div>
--- packages/udesign-ui/src/components/radio/group.tsx
+++ packages/udesign-ui/src/components/radio/group.tsx
@@ -1,29 +1,29 @@
 import React, { isValidElement, ReactNode, useEffect, useReducer } from 'react';
 import classNames from 'classnames';
-import { NativeProps } from '../../utils';
+import { NativeProps, usePropsValue } from '../../utils';
 import { Radio } from './radio';
-import { RadioContext, reducer, types } from './context';
+import { RadioContext } from './context';
 import { BASE_CLASS_PREFIX } from '../../constants';
 
 export type RadioOptionType = {
   label: string; // 对外显示
   value: string; // 真实值
   disabled?: boolean;
-  icon?: ReactNode; // 自定义图标。默认值：-
+  icon?: ReactNode; // 自定义图标。
 };
 
 export type GroupProps = {
   defaultValue?: string; // 默认选中的值。默认值：''
-  value?: string; // 用于设置当前选中的值。默认值：-
+  value?: string; // 用于设置当前选中的值。
   disabled?: boolean; // 禁选所有子单选器。默认值：false
-  options?: Array<RadioOptionType | string>; // 以配置形式设置子元素。默认值：-
-  name: string; // RadioGroup 下所有 input[type="radio"] 的 name 属性。默认值：-
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void; // 选项变化时的回调函数。默认值：-
+  options?: Array<RadioOptionType | string>; // 以配置形式设置子元素。
+  name: string; // RadioGroup 下所有 input[type="radio"] 的 name 属性。
+  onChange?: (event: React.MouseEvent<HTMLInputElement>) => void; // 选项变化时的回调函数。
 } & NativeProps;
 
 const prefixCls = `${BASE_CLASS_PREFIX}-radio`;
 
-export const Group = ({ defaultValue = '', disabled, options, children, name, onChange, className, style, ...restProps }: GroupProps) => {
+export const Group = ({ defaultValue = '', disabled, options, children, name, className, style, ...restProps }: GroupProps) => {
   // 用于支持简单的数组作为options
   const getOptions = () => {
     if (!options) return [];
@@ -38,18 +38,12 @@ export const Group = ({ defaultValue = '', disabled, options, children, name, on
     });
   };
 
-  const wrapperClass = classNames(`${prefixCls}-wrapper`, className);
-  const initialState = { value: restProps.value };
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [value, setValue] = usePropsValue({
+    value: restProps.value,
+    defaultValue,
+  });
 
-  useEffect(() => {
-    dispatch({
-      type: types.UPDATE_VALUE,
-      payload: {
-        value: restProps.value || defaultValue,
-      },
-    });
-  }, [restProps.value, defaultValue]);
+  const wrapperClass = classNames(`${prefixCls}-wrapper`, className);
 
   const renderChildren = () => {
     if (children) return isValidElement(children) ? children : <>{children}</>;
@@ -57,19 +51,24 @@ export const Group = ({ defaultValue = '', disabled, options, children, name, on
       const radioProps =
         'value' in restProps
           ? {
-              checked: restProps.value === option.value,
+              checked: value === option.value,
             }
           : {};
       return (
-        <Radio value={option.value} icon={option.icon} key={option.value.toString()} defaultChecked={defaultValue === option.value} disabled={'disabled' in option ? option.disabled : disabled} {...radioProps}>
+        <Radio value={option.value} icon={option.icon} key={option.value.toString()} defaultChecked={defaultValue === option.value} disabled={'disabled' in option ? option.disabled : disabled} checked={value === option.value}>
           {option.label}
         </Radio>
       );
     });
   };
 
+  const onChange = (val: string, e: React.MouseEvent<HTMLInputElement>) => {
+    setValue(val);
+    restProps.onChange?.(e);
+  };
+
   return (
-    <RadioContext.Provider value={{ value: state.value, name, disabled, onChange, dispatch }}>
+    <RadioContext.Provider value={{ name, disabled, value, onChange }}>
       <div className={wrapperClass} style={style}>
         {renderChildren()}
       </div>
