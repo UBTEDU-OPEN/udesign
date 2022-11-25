b8e5c6ec4176507b47334c3115fad7ca35ff724c
xianggang.shen <xianggang.shen@ubtrobot.com>
2022-11-25 10:48:01 +0800

    fix: 修复Checkbox的受控功能
    Change-Id: Id78e450cb653a88825a77d06f63663a286363fc3

--- packages/udesign-ui/src/components/checkbox/context.ts
+++ packages/udesign-ui/src/components/checkbox/context.ts
@@ -1,28 +1,9 @@
 import { createContext, Context } from 'react';
 
-export const enum types {
-  UPDATE_VALUE = 'UPDATE_VALUE',
-}
-
-export interface actionType {
-  type: string;
-  payload: any;
-}
-
-export function reducer(state: { [key: string]: any }, action: actionType) {
-  switch (action.type) {
-    case types.UPDATE_VALUE:
-      return action.payload;
-    default:
-      throw new Error();
-  }
-}
-
 export interface CheckboxContextState {
   value?: string[];
   name?: string;
-  onChange?: (checkedValue: string[]) => void;
-  dispatch?: React.Dispatch<actionType>;
+  onChange?: (e: string[]) => void;
   disabled?: boolean;
 }
 
--- packages/udesign-ui/src/components/checkbox/group.tsx
+++ packages/udesign-ui/src/components/checkbox/group.tsx
@@ -1,30 +1,30 @@
-import React, { useEffect, useReducer, useState, isValidElement, ReactNode } from 'react';
+import React, { isValidElement, ReactNode } from 'react';
 import classNames from 'classnames';
-import { NativeProps } from '../../utils';
+import { NativeProps, usePropsValue } from '../../utils';
 import { Checkbox } from './checkbox';
-import { CheckboxContext, reducer, types } from './context';
+import { CheckboxContext } from './context';
 import { BASE_CLASS_PREFIX } from '../../constants';
 
 export type CheckboxOptionType = {
-  label: string; // 对外显示。默认值：-
-  value: string; // 真实值。默认值：-
+  label: string; // 对外显示。
+  value: string; // 真实值。
   disabled?: boolean; // 是否禁用。默认值：false
-  icon?: ReactNode; // 自定义图标。默认值：-
+  icon?: ReactNode; // 自定义图标。
 };
 
 export type GroupProps = {
   defaultValue?: string[]; // 默认选中的值。默认值：[]
-  value?: string[]; // 用于设置当前选中的值。默认值：-
+  value?: string[]; // 用于设置当前选中的值。
   disabled?: boolean; // 禁选所有子多选器。默认值：false
-  options?: Array<CheckboxOptionType | string>; // 以配置形式设置子元素。默认值：-
-  name: string; // CheckboxGroup 下所有 input[type="radio"] 的 name 属性。默认值：-
-
-  onChange?: (checkedValue: string[]) => void; // 选项变化时的回调函数  function(checkedValue)。默认值：-
+  options?: Array<CheckboxOptionType | string>; // 以配置形式设置子元素。
+  name: string; // CheckboxGroup 下所有 input[type="radio"] 的 name 属性。
+  onChange?: (checkedValue: string[]) => void; // 选项变化时的回调函数。
 } & NativeProps;
 
 const prefixCls = `${BASE_CLASS_PREFIX}-checkbox`;
 
-export const Group = ({ defaultValue = [], disabled, options, children, name, style, className, onChange, ...restProps }: GroupProps) => {
+export const Group = (props: GroupProps) => {
+  const { defaultValue = [], disabled, options, children, name, style, className } = props;
   // 用于支持简单的数组作为options
   const getOptions = () => {
     if (!options) return [];
@@ -40,46 +40,36 @@ export const Group = ({ defaultValue = [], disabled, options, children, name, st
   };
 
   const wrapperClass = classNames(`${prefixCls}-wrapper`, className);
-  const initialState = { value: restProps.value || [] };
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const [innerDefaultValue, setInnerDefaultValue] = useState<string[]>(defaultValue!);
 
-  useEffect(() => {
-    dispatch({
-      type: types.UPDATE_VALUE,
-      payload: {
-        value: restProps.value && Array.isArray(restProps.value) && restProps.value.length > 0 ? restProps.value : [],
-      },
-    });
-  }, [restProps.value]);
-
-  useEffect(() => {
-    dispatch({
-      type: types.UPDATE_VALUE,
-      payload: {
-        value: innerDefaultValue && Array.isArray(innerDefaultValue) && innerDefaultValue.length > 0 ? innerDefaultValue : [],
-      },
-    });
-  }, [innerDefaultValue]);
+  const [value, setValue] = usePropsValue({
+    value: props.value,
+    defaultValue,
+    onChange: props.onChange,
+  });
 
   const renderChildren = () => {
     if (children) return isValidElement(children) ? children : <>{children}</>;
     return getOptions()?.map((option: CheckboxOptionType) => {
       const radioProps =
-        'value' in restProps
+        'value' in props
           ? {
-              checked: state.value.includes(option.value),
+              checked: value.includes(option.value),
             }
           : {};
       return (
-        <Checkbox value={option.value} icon={option.icon} key={option.value.toString()} defaultChecked={defaultValue.includes(option.value)} disabled={'disabled' in option ? option.disabled : disabled} {...radioProps}>
+        <Checkbox value={option.value} icon={option.icon} key={option.value.toString()} defaultChecked={value.includes(option.value)} disabled={'disabled' in option ? option.disabled : disabled} {...radioProps}>
           {option.label}
         </Checkbox>
       );
     });
   };
+
+  const onChange = (e: string[]) => {
+    setValue(e);
+  };
+
   return (
-    <CheckboxContext.Provider value={{ value: state.value, name, onChange, dispatch, disabled }}>
+    <CheckboxContext.Provider value={{ value, name, onChange, disabled }}>
       <div className={wrapperClass} style={style}>
         {renderChildren()}
       </div>
