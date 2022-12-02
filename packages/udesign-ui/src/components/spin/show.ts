12382acf971611e4b67b8cd30386674ef8fe5a8f
Arvin <webappsniper@gmail.com>
2022-12-02 11:32:58 +0800

    refactor: 修改Spin组件的全局加载超时时间为120秒，并增加文档演示
    Change-Id: Idce89cc6dc817480433051ef6fbfcccce4086597

--- packages/udesign-ui/src/components/spin/show.ts
+++ packages/udesign-ui/src/components/spin/show.ts
@@ -4,13 +4,22 @@ import './spin.scss';
 
 const element = new PortalWrapper<typeof InnerSpin>(InnerSpin);
 
+let timer: NodeJS.Timeout;
+
 export function showLoading(props?: SpinProps) {
+  clearTimeout(timer);
+  const timeout = props?.timeout || 120000;
+
   element.show({
-    timeout: 60000,
+    timeout: 0,
     mask: true,
     fullscreen: true,
     ...props,
   });
+
+  timer = setTimeout(() => {
+    element.close();
+  }, timeout);
 }
 export function hideLoading() {
   element.close();
