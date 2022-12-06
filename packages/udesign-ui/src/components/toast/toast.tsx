d3563e67be8881562145143200839148835cb266
xianggang.shen <xianggang.shen@ubtrobot.com>
2022-12-06 10:00:40 +0800

    fix: toast组件的position新增四个方向
    Change-Id: Id070c3160f46b67b0237a79a0349a877e5359794

--- packages/udesign-ui/src/components/toast/toast.tsx
+++ packages/udesign-ui/src/components/toast/toast.tsx
@@ -9,7 +9,7 @@ export type ToastProps = {
   delay?: number; // 延时出现时间，单位ms。默认值：1000ms
   content: React.ReactNode; // 显示内容 React.ReactNode。默认值：-
   duration?: number; // 提示持续时间。默认值：2000
-  position?: 'top' | 'bottom' | 'center' | 'left' | 'right'; // 提示框出现的位置。默认值：top
+  position?: 'top' | 'bottom' | 'center' | 'left' | 'right' | 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight'; // 提示框出现的位置。默认值：top
   zIndex?: number; // z-index 值。默认值：1010
   afterClose?: () => void; // 关闭后的回调。默认值：-
 } & NativeProps;
